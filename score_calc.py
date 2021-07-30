5bf46fc47bf6cd66b35823d1840e7524315b66e3
guest <walter.sessions@gmail.com>
2021-07-30 11:07:28 -0400

    broke options out into yaml

--- score_calc.py
+++ score_calc.py
@@ -9,73 +9,9 @@ import os
 import csv
 import numpy as np
 import requests
+import yaml
 
-namelist = {
-    #_list of countries in play
-    'countries' : [
-        'United States',                    #_0 
-        'Azerbaiyán',                       #_1
-        'Brazil',                           #_2
-        'Croatia',                          #_3
-        'Uzbekistán',                       #_4
-        'Greece',                           #_5
-        'France',                           #_6
-        'Italy',                            #_7
-        'China',                            #_8
-        'Japan',                            #_9
-        'Netherlands',                      #_10
-        'Poland',                           #_11
-        'Turkey',                           #_12
-        'India',                            #_13
-        'Chinese Taipei',                   #_14
-        'South Korea',                      #_15
-        'Germany',                          #_16
-        'Finland',                          #_17
-        'Sweden',                           #_18
-        'Belgium',                          #_19
-        'Argentina',                        #_20
-        'Independent Olympic Participant',  #_21
-        'Republic of Irland',               #_22
-        'Australia',                        #_23
-        'Mexico',                           #_24
-        'Philippines',                      #_25
-        'Spain',                            #_26
-        'United Kingdom',                   #_27
-        'Jamaica',                          #_28
-        'Canada',                           #_29
-        'Russian Olympic Committee',        #_30 
-        'Thailand',                         #_31    
-        'Denmark',                          #_32
-        'Austria' ],                        #_33
-
-    #_player picks from above
-    'players' : {
-        'Rob'       : [0,31,1,32,2,3,4,5],
-        'Jared'     : [0,6,7],
-        'Ben'       : [8,9,7,10,11,12,13,14],
-        'Marquis'   : [0,9,15],
-        'Walter'    : [8,9,16,17,18,19,20,21],
-        'Ramon'     : [8,30,10,11,19,22,14,13],
-        'Jeannette' : [23,2,16,9,6,24,25,26],
-        'Josh'      : [27,28,8,29],
-        'Zach'      : [0,30,33] },
-
-    #_scoring of medals
-    'values' : {
-        'gold'      : 7, 
-        'silver'    : 3, 
-        'bronze'    : 1 },
-
-    #_discord channel url
-    'channel_name' : 'rob',
-    'channels' : {
-        'test': 'https://discord.com/api/v9/channels/xxxxxxxxxxx/messages',
-        'rob' : 'https://discord.com/api/v9/channels/xxxxxxxxxxx/messages'   },
-
-    #_user auth code for discord
-    'discord_auth' : 'mfa.xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
-
-    }
+namelist = yaml.load(open('options.yaml', 'r') Loader=yaml.SafeLoader)
 
 #############################################################################_80
 ################################################################################
